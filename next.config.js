421281547f42f7d280266857c4e75c3e25e9025a
Tom Söderlund <tom@tomsoderlund.com>
2021-04-09 09:51:42 +0200

    i18n setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  i18n: {
+    locales: ['en', 'se', 'site1', 'site2'],
+    defaultLocale: 'en',
+    domains: [
+      {
+        domain: 'multi-domain-locale1.vercel.app',
+        defaultLocale: 'site1'
+      },
+      {
+        domain: 'multi-domain-locale2.vercel.app',
+        defaultLocale: 'site2'
+      }
+    ]
+  },
+
   future: {
     webpack5: true
   }
