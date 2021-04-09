c5adad56c2b7c51b9bad3e1543553ea4d2bd8bb2
Tom Söderlund <tom@tomsoderlund.com>
2021-04-09 10:18:32 +0200

    next/config – publicRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  publicRuntimeConfig: {
+    // Will be available on both server and client - restart needed to change
+    sites: {
+      site1: { title: 'The 1st Site', description: 'This is the first site.' },
+      site2: { title: 'The 2nd Site', description: 'This is the second site.' }
+    }
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side - restart needed to change
+  },
+
   i18n: {
     locales: ['en', 'se', 'site1', 'site2'],
     defaultLocale: 'en',
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import getConfig from 'next/config'
 
 import styles from '../styles/Home.module.css'
+import packageJson from '../package.json'
 
 export default function Home () {
   const { locale, locales, defaultLocale } = useRouter()
+  const { publicRuntimeConfig: { sites } } = getConfig()
   return (
     <div className={styles.container}>
       <Head>
         <title>Multi Domain using i18n</title>
+        <meta name='description' content={packageJson.description} />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Site: {locale}</h1>
+        <h1 className={styles.title}>{sites[locale] ? sites[locale].title : `Site not found: '${locale}'`}</h1>
+        {sites[locale] && (
+          <p className={styles.description}>{sites[locale].description}</p>
+        )}
+
+        <p className={styles.description}>{packageJson.description}</p>
 
         <div className={styles.grid}>
           <div className={styles.card}>
