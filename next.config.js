168dc393321e6574007b76a3323b1c68b6a97b61
Tom Söderlund <tom@tomsoderlund.com>
2021-04-09 13:46:13 +0200

    Prettier config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
+// Next.js’ i18n `locale` is used to determine the site:
+const sites = {
+  site1: { title: 'The 1st Site', description: 'This is the description for the first site.' },
+  site2: { title: 'The 2nd Site', description: 'This is the description for the second site.' }
+}
+const siteKeys = Object.keys(sites)
+
+// Selecting a language is instead handled via a `pseudoLocale` prop:
+const pseudoLocales = ['en', 'se']
+
 module.exports = {
+  // next/config: publicRuntimeConfig available on both server and client – restart needed to change
   publicRuntimeConfig: {
-    // Will be available on both server and client - restart needed to change
-    sites: {
-      site1: { title: 'The 1st Site', description: 'This is the description for the first site.' },
-      site2: { title: 'The 2nd Site', description: 'This is the description for the second site.' }
-    },
-    // Selecting a language is instead handled via a `pseudoLanguage` prop:
-    pseudoLocales: ['en', 'se']
+    sites,
+    pseudoLocales
   },
 
   // We "hijack" Next.js’ i18n system to use for different sites
   i18n: {
-    locales: ['site1', 'site2'],
-    defaultLocale: 'site1',
+    locales: siteKeys,
+    defaultLocale: siteKeys[0],
     domains: [
       {
         domain: 'multi-domain-locale1.vercel.app',
@@ -25,12 +31,12 @@ module.exports = {
     ]
   },
 
-  // Redirect / to pseudoLocale: en
+  // Redirect / to default pseudoLocale
   redirects: () => (
     [
       {
         source: '/',
-        destination: '/en',
+        destination: `/${pseudoLocales[0]}`,
         permanent: true
       }
     ]
